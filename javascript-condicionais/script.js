eb760de799ab620d16023799dcc5c795e218a021
Gabriel Rossini <gmrtec@proton.me>
2026-07-29 17:45:40 -0300

    aula 1 de condicionais

---
+++ javascript-condicionais/script.js
@@ -0,0 +1,8 @@
+const condicao = Boolean(true);
+console.log(condicao);
+
+const stringPura = Boolean("");
+console.log(stringPura);
+
+const string1Caracter = Boolean(" ");
+console.log(string1Caracter);
