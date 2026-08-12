e769c41214e1fc9321749ff921eb20db287bf483
Gabriel Rossini <gmrtec@proton.me>
2026-08-12 02:25:11 -0300

    fim da aula de arrays e loops

--- javascript-arrays/script.js
+++
@@ -1,9 +0,0 @@
-const lista = ["JavaScript", "HTML", "CSS", "NODE.JS"];
-
-const body = document.querySelector("body");
-
-function adicionarBody(i) {
-  body.innerHTML += "<li>" + i + "</li>";
-}
-
-lista.forEach(adicionarBody);
